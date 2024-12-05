3b45c54a6c279b0f1bb6c859247cf45d588b7c2a
oloturia <5429234+oloturia@users.noreply.github.com>
2024-12-05 16:56:12 +0100

    day 5 complete

---
+++ 05/part1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def main(input_file):
+	with open(input_file) as f:
+		input_lines = [line.rstrip() for line in f.readlines()]
+
+	rules = dict()
+	result = 0
+	for line in input_lines:
+		if '|' in line:
+			if line.split('|')[0] not in rules:
+				rules[line.split('|')[0]] = [line.split('|')[1]]
+			else:
+				rules[line.split('|')[0]].append(line.split('|')[1])
+		
+		elif ',' in line:
+			found = True
+			i = 0
+			while found and i < len(line.split(',')):
+				num = line.split(',')[i]
+				for nump in line.split(',')[:i]:
+					if num not in rules:
+						break
+					if nump in rules[num]:
+						found = False
+						break
+				i += 1
+			if found:
+				result += int( line.split(',')[len(line.split(','))//2] )
+		
+
+	return result
+
+if __name__ == "__main__":
+	test_value = main("TEST")
+	expected_result = 143
+	assert test_value == expected_result,f"Test failed, expected {expected_result}, result {test_value}"
+	print(main("INPUT"))
---
+++ 05/part2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+
+def tester(rules,linesp):
+	found = True
+	i = 0
+	while found and i < len(linesp):
+		num = linesp[i]
+		for nump in linesp[:i]:
+			if num not in rules:
+				break
+			if nump in rules[num]:
+				found = False
+				break
+		i += 1
+	if not found:
+		return (nump,num)
+	else:
+		return False
+
+def main(input_file):
+	with open(input_file) as f:
+		input_lines = [line.rstrip() for line in f.readlines()]
+
+	rules = dict()
+	result = 0
+	for line in input_lines:
+		if '|' in line:
+			if line.split('|')[0] not in rules:
+				rules[line.split('|')[0]] = [line.split('|')[1]]
+			else:
+				rules[line.split('|')[0]].append(line.split('|')[1])	
+		elif ',' in line:
+			newline = line.split(',').copy()
+			found = tester(rules,newline)
+			if not found:
+				continue
+			while found:
+				temp = newline.pop(newline.index(found[0]))
+				newline.insert(newline.index(found[1])+1,temp)
+				found = tester(rules,newline)
+			
+			result += int( newline[len(newline)//2] )
+
+	return result
+
+if __name__ == "__main__":
+	test_value = main("TEST")
+	expected_result = 123
+	assert test_value == expected_result,f"Test failed, expected {expected_result}, result {test_value}"
+	print(main("INPUT"))
